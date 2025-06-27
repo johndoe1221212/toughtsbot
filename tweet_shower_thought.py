71c604664a58666d9ae1d97a18221a5dc582149d
johndoe1221212 <lmaowhy322@gmail.com>
2025-06-27 03:15:01 -0700

    Update tweet_shower_thought.py

--- tweet_shower_thought.py
+++ tweet_shower_thought.py
@@ -1,20 +1,21 @@
-import os
 import praw
 import tweepy
 import random
 import sys
 from datetime import datetime, timezone
 
+# Reddit credentials (hardcoded)
 reddit = praw.Reddit(
-    client_id=os.getenv('REDDIT_CLIENT_ID'),
-    client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
-    user_agent=os.getenv('REDDIT_USER_AGENT')
+    client_id='ezPbHCuMJj1ChN3AgnnNsQ',
+    client_secret='Fo-_z2FPZU7cnkASLULylNDEJ0jCMQ',
+    user_agent='ShowerThoughtsBot by u/Typical_Farm6742'
 )
 
-consumer_key = os.getenv('TWITTER_CONSUMER_KEY')
-consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET')
-access_token = os.getenv('TWITTER_ACCESS_TOKEN')
-access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+# Twitter credentials (hardcoded)
+consumer_key = 'JYAsQiOuOQQavkxN4B1Uf1kch'
+consumer_secret = '9kEoZ5UfxsDok8OP6QY9GdtxaoNEQPBTugcdMVP8yBtdNbE05Q'
+access_token = '1938321275136442368-zGLGpqRpoU6yolyUMPXsVUZdMjwgXS'
+access_token_secret = 'Pu0O9kSnDH6rPykLzzQ0eoIyDHAsnQePOtuzCJve8XLrN'
 
 auth = tweepy.OAuth1UserHandler(
     consumer_key, consumer_secret, access_token, access_token_secret
@@ -45,4 +46,5 @@ def tweet_showerthought():
 
 if __name__ == "__main__":
     tweet_showerthought()
+
 
